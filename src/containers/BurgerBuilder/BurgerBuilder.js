2bfc3af087b70ae84ba8ebafbf9402912896fd07
yudai yamashita <yama226.33@gmail.com>
2018-09-09 08:34:56 +0900

    材料追加ボタンの処理を実装

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/aux';
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENTS_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    becon: 0.7
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -11,13 +18,31 @@ class BurgerBuilder extends Component {
             bacon: 2,
             cheese: 3,
             meet: 0,
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+
+        updatedIngredients[type] = updatedCount;
+        const priceAddtion = INGREDIENTS_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddtion;
+
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients})
+
     }
     render () {
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredentAdded={this.addIngredientHandler} />
             </Aux>
         )
     }
