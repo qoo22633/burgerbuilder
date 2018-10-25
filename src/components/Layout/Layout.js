b6ec273e7aff0bed01687096ea380cf13768a4e3
yudai yamashita <yama226.33@gmail.com>
2018-10-25 21:29:32 +0700

    メニュートグル機能の実装 #141まで

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-import Logo from '../Logo/Logo';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    drawerToggleClicked = () => {
+        this.setState( (prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Aux>
+                < Toolbar drawerToggleClicked = {this.drawerToggleClicked}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+export default Layout;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" active>Buger Builder</NavigationItem>
-        <NavigationItem link="/" active>Checkout</NavigationItem>
+        <NavigationItem link="/" >Checkout</NavigationItem>
     </ul>
 );
 
