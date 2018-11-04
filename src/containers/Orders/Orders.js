8cff3c645a25ebce5a4d63b8807cf1672ae02330
yudai yamashita <yama226.33@gmail.com>
2018-11-04 12:47:21 +0700

    orderをDBから引っ張って表示する処理を実装 #222

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -13,15 +13,14 @@ class Orders extends Component {
     componentDidMount () {
         axios.get('/orders.json')
             .then(res => {
-                const featchedOrders = [];
+                const fetchedOrders = [];
                 for (let key in res.data) {
-                    featchedOrders.push({
+                    fetchedOrders.push({
                         ...res.data[key],
                         id: key
                     });
                 }
-                console.log(res.data);
-                this.setState({ loading: false, orders: featchedOrders })
+                this.setState({ loading: false, orders: fetchedOrders })
             })
             .catch(err => {
                 this.setState({ loading: false })
@@ -31,8 +30,12 @@ class Orders extends Component {
     render () {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         )
     }
