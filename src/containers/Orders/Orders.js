b0c95444ef1be548c283fcf3cdcf749ce21cc20b
yudai yamashita <yama226.33@gmail.com>
2018-11-04 10:45:43 +0700

    import先の修正

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Order from '../../../components/Order/Order';
+import Order from '../../components/Order/Order';
 
 class Orders extends Component {
     render () {
