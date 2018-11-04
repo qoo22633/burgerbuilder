8b16a225d1a23d077db3ac920b7274ce7960d414
yudai yamashita <yama226.33@gmail.com>
2018-11-04 11:10:49 +0700

    ordersをDBからfetchするように実装 #221

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount () {
+        axios.get('/orders.json')
+            .then(res => {
+                const featchedOrders = [];
+                for (let key in res.data) {
+                    featchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                console.log(res.data);
+                this.setState({ loading: false, orders: featchedOrders })
+            })
+            .catch(err => {
+                this.setState({ loading: false })
+            });
+    }
+
     render () {
         return (
             <div>
@@ -13,4 +38,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
